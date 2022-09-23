4bd37791a152b28d142a670f7cced59bba60a17b
Thiago <thimarsilva18@gmail.com>
2022-09-23 12:07:48 -0300

    First deploy

---
+++ web/src/Components/Resevation.tsx
@@ -0,0 +1,12 @@
+import { Popover } from "@headlessui/react";
+
+export function Resevation () {
+    return (
+        <Popover.Panel className="relative">
+            <Popover.Button>Resevation</Popover.Button>
+
+            <p className="bg-black">hjfsdf</p>
+        </Popover.Panel>
+    )
+        
+}
---
+++ web/tailwind.config.js
@@ -0,0 +1,16 @@
+// @type {import('tailwindcss').Config} 
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'title-color': '#44cd5e',
+        'background-color': '#111111'
+      },
+    },
+  },
+  plugins: [],
+}
